47167bb2afe3cebc71e23156033ef615a2203ef1
amansin0504 <singh.amandeep@hotmail.co.in>
2023-03-10 18:31:37 -0500

    url frontend update

--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -4,7 +4,7 @@ sudo apt install python3-pip -y
 sudo apt install wget
 sudo pip install Flask
 
-git clone https://github.com/amansin0504/tfm-demo-app-azure-vm.git
+git clone https://github.com/amansin0504/tfm-demo-app-gcp-vm.git
 mkdir app/
 mkdir app/templates
 cp tfm-demo-app-azure-vm/source/frontend.py app/app.py
